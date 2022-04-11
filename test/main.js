83768232be8065dc33ae691572fdf4ba8bfa99e8
diego <admin@dbltecnologia.com.br>
2022-04-11 13:20:31 -0300

    Changes on test script

--- test/main.js
+++ test/main.js
@@ -6,6 +6,7 @@ const { ethers } = require("hardhat");
 const { utils } = require('ethers');
 const { expectRevert } = require('@openzeppelin/test-helpers');
 
+
 let dev, user, none, alice, bob, charlie, devAddress, userAddress, noneAddress, aliceAddress, bobAddress;
 let main;
 let postDeployDevBalance;
@@ -30,8 +31,8 @@ describe("main", function () {
             noneAddress = none.address,
             aliceAddress = alice.address,
             bobAddress = bob.address;
-        const _main = await ethers.getContractFactory("Main");
-        main = await _main.deploy(dev.address, dev.address);
+        const _main = await ethers.getContractFactory("MainMulti");
+        main = await _main.deploy(true, dev.address);
         await main.deployed();
         postDeployDevBalance = await main.provider.getBalance(dev.address);
     });
@@ -40,7 +41,7 @@ describe("main", function () {
     describe("Check for proper deposit", () => {
         it("Should only allow deposit greater than minDelegate", async () => {
             // Will revert if deposit is less thatn minimum required.
-            await expectRevert(main.connect(alice).deposit({ value: FIVE }), 'Can only delegate a minimum of 100 ONE');
+            await expectRevert(main.connect(alice).deposit({ value: FIVE }), 'Invalid ONE amount set, send more ONE');
         });
     });
 
@@ -70,42 +71,36 @@ describe("main", function () {
         });
 
         it("Should emit an event when validator address is changed", async () => {
-            const validatorAddress = await main.validatorAddress();
-            await expect(main.changeValidator(alice.address)).to.emit(main, "validatorChanged").withArgs(validatorAddress, alice.address);
+            const defaultValidator = await main.defaultValidator();
+            await expect(main.changeValidator(alice.address)).to.emit(main, "validatorChanged").withArgs(defaultValidator, alice.address);
         });
-
-        it("Should emit an event when validator address is changed", async () => {
-            const validatorAddress = await main.validatorAddress();
-            await expect(main.changeValidator(alice.address)).to.emit(main, "validatorChanged").withArgs(validatorAddress, alice.address);
-        });
-
-
-
-
-        //TODO: add event test for withdrawEpochsChanged - ok
-        //TODO: add event test for withdrawTimestampChanged - ok
-        //TODO: add event test for CollectReward - ok
-        //TODO: add event test for Withdraw - not ok
-        //TODO: add event test for Unstake - ok
-        //TODO: add event test for deposit - ok
-        // TODO: CHECK THIS: await expect(await main.unstake(1)).to.emit(main, "Unstake");
+        //TODO: add event test for withdrawEpochsChanged
+        //TODO: add event test for withdrawTimestampChanged
+        //TODO: add event test for CollectReward
+        //TODO: add event test for Withdraw
+        //TODO: add event test for Unstake
+        //TODO: add event test for deposit
 
         it("Should emit an withdrawEpochsChanged event when the epoch of withdraw has changed", async () => {                                              
-            const now = await main.getwithdrawEpochs()            
+            const now = await main.withdrawEpochs()
+
+            console.log('now ', now)
             await expect(main.changeWithdrawEpoch(now)).to.emit(main, "withdrawEpochsChanged").withArgs(now, now);
         });
 
         it("Should emit an withdrawTimestampChanged event when the timestamp of withdraw has changed", async () => {                                              
-            const now = await main.getwithdrawTimestamp()            
+            const now = await main.withdrawTimestamp()
             await expect(main.changeWithdrawTimestamp(now)).to.emit(main, "withdrawTimestampChanged").withArgs(now, now);
         });
 
         it("Should emit an CollectReward event when a deposit is made", async () => {                           
-            await expect(await main.connect(alice).deposit({ value: ONE_H })).to.emit(main, "CollectReward");
+            await expect(await main.advance()).to.emit(main, "CollectReward");
         });
 
-        it("Should emit an Withdraw event when a withdraw is made", async () => {                                       
-            // await expect(main.withdrawAll()).to.emit(main, "Withdraw");
+        it("Should emit an Withdraw event when a withdraw is made", async () => {   
+            
+             await main.changeWithdrawEpoch(0)
+             await expect(main.withdrawAll()).to.emit(main, "Withdraw");
         });
 
         it("Should emit an Unstake event when a Unstake is made", async () => {                                       
@@ -114,18 +109,16 @@ describe("main", function () {
             await expect(main.unstake(balance)).to.emit(main, "Unstake");
         });
 
-        it("Should emit an Deposit event when a deposit is made", async () => {                             
+        it("Should emit an Deposit event when a deposit is made", async () => {    
             await expect(main.connect(alice).deposit({ value: ONE_H })).to.emit(main, "Deposit");
         });
-                                        
+
+        it("Should emit an Advance event when a advance is called", async () => {    
+            await expect(main.advance()).to.emit(main, "Advance");
+        });
+           
+
     });
-
-
-
-    
-
-
-
 
     describe("When all user unstake all deposits", () => {
 
@@ -147,7 +140,7 @@ describe("main", function () {
         });
 
         xit("Contract balance should be zero", async function () {
-            // Check contract balance 
+            // Check contract balance
             let balance = parseInt(fromWei(await main.provider.getBalance(main.address)));
             expect(balance).to.be.eq(0);
         });
@@ -220,11 +213,8 @@ describe("main", function () {
         });
 
         it("sOne value should be greater than ONE once reward started.", async function () {
-
             // simulate rewards
-
-            // Error: Transaction reverted: function selector was not recognized and there's no fallback nor receive function
-             // await none.sendTransaction({ to: main.address, value: ethers.utils.parseEther("100") });
+            await none.sendTransaction({ to: main.address, value: ethers.utils.parseEther("100") });
 
             // charlie user deposit
             await main.connect(charlie).deposit({ value: ONE_H });
